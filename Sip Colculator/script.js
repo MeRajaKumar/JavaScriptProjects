5e316c6dd248c4448c0cb65d7b57be6b078d16b5
MeRajaKumar <teamaayushgupta@gmail.com>
2023-12-16 21:14:59 +0530

    all changes for the day

---
+++ Sip Colculator/script.js
@@ -0,0 +1,4 @@
+var monthly_investment = document.getElementById('monthly-investment');
+var return_rate = document.getElementById('return-rate');
+var time_period = document.getElementById('time-period');
+
