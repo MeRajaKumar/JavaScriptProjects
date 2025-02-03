c79f5708273953a67f8534279ede0a2766014413
Raja Kumar <teamaayushgupta@gmail.com>
2025-02-04 00:00:49 +0530

    all changes for the day

---
+++ Sign-up Form Validation/script.js
@@ -0,0 +1,9 @@
+var fullname = document.getElementById('fullname').value;
+var mobile = document.getElementById('mobile').value;
+var email = document.getElementById('email').value;
+var pass = document.getElementById('password').value;
+var re_pass = document.getElementById('re-password').value;
+
+if(fullname === ""){
+    
+}
