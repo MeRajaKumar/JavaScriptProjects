cff2f3ffda6e1994003be8dc688e93ddb0da63ab
Raja Kumar <teamaayushgupta@gmail.com>
2025-09-03 13:15:37 +0530

    initial commit

---
+++ popup-message/script.js
@@ -0,0 +1,9 @@
+let popus = document.getElementById("popup");
+
+function openPopus(){
+    popus.classList.add("open-popup");
+}
+
+function closePopus(){
+    popus.classList.remove("open-popup");
+}
